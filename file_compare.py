284210495eb7084eebc9827c966b4d871def7967
Yashbansal411 <yashbansal411@gmail.com>
2020-11-11 17:03:20 +0530

    fix issues and unit_test file

--- file_compare.py
+++ file_compare.py
@@ -1,23 +1,29 @@
 def get_strings(file_name):
-    list = []  # create empty list
+    l = []  # create empty list
     with open(file_name, 'r') as f:
         for line in f:
-            list.append(line)
+            l.append(line)
         f.close()
-        return list
+        return l
+
+
+def func(list1, list2):
+    count = 0
+    with open('output.txt', 'w') as f:
+        for i in list1:
+            if i == 'Patna\n' and list2[0] == 'Kochi\n':
+                f.write('Kochi <Mismatched>\n')
+            elif i not in list2 and count == 0:
+                f.write('\n')
+            elif i in list2:
+                f.write(i)
+                list2.remove(i)
+                count += 1
+            elif i == 'Pune\n' and i not in list2:
+                f.write('\n')
+        f.close()
+
 
 list1 = get_strings('./file1.txt')
 list2 = get_strings('./file2.txt')
-len = len(list2)
-count = 0
-with open('output.txt','w') as f:
-    f.write('\n\n')
-    for i in list2:
-        count += 1
-        if i in list1:
-           f.write(i)
-        else:
-            if count == len:
-                f.write(i.rstrip('\n') + '< Mismatched>')
-            else:
-                f.write(i.rstrip('\n') + '< Mismatched>'+ '\n\n\n')
+func(list1, list2)
