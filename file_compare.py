ceb33fe884a9aad79ff922526a0c143ab677dff0
Yashbansal411 <yashbansal411@gmail.com>
2020-11-17 18:57:51 +0530

    no hard code

--- file_compare.py
+++ file_compare.py
@@ -1,3 +1,4 @@
+from _collections import OrderedDict
 def get_strings(file_name):
     l = []  # create empty list
     with open(file_name, 'r') as f:
@@ -8,19 +9,19 @@ def get_strings(file_name):
 
 
 def func(list1, list2):
-    count = 0
+    od = OrderedDict()
+    len1 = len(list1)
+    count = 1
     with open('output.txt', 'w') as f:
         for i in list1:
-            if i == 'Patna\n' and list2[0] == 'Kochi\n':
-                f.write('Kochi <Mismatched>\n')
-            elif i not in list2 and count == 0:
+            if count == len1 and i not in list2:
+                f.write(list2[0].rstrip('\n')+'<mismatch>')
+            elif i not in list2:
                 f.write('\n')
-            elif i in list2:
+            else:
                 f.write(i)
                 list2.remove(i)
-                count += 1
-            elif i == 'Pune\n' and i not in list2:
-                f.write('\n')
+            count += 1
         f.close()
 
 
