2279b4f3495a9855810513f2961f613895898a49
Yashbansal411 <yashbansal411@gmail.com>
2020-12-22 18:27:54 +0530

    both inputs are inputs and unit testing file

--- unit_testing_file.py
+++ unit_testing_file.py
@@ -1,38 +1,83 @@
-import fileCompare26nov
+from file_compare_both_inputs_sorted import main_func
 
-def test_case1():
-    list1 = ['Pune\n', 'Maharashtra\n', 'Mumbai\n', 'Delhi\n', 'Patna\n']
-    list2 = ['Mumbai\n', 'Delhi\n', 'Kochi\n']
-    list3 = fileCompare26nov.func(list1, list2)
-    assert list3 == ['\n', '\n', 'Mumbai\n', 'Delhi\n', 'Kochi<mismatch>\n']
+def test_exact_same():
+    list1 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"}]
 
-def test_case2():
-    list1 = ['Pune\n', 'Mumbai\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n']
-    list2 = ['Mumbai\n', 'Delhi\n', 'Kochi\n']
-    list3 = fileCompare26nov.func(list1, list2)
-    assert list3 == ['\n', 'Mumbai\n','\n', 'Delhi\n', 'Kochi<mismatch>\n']
+    list2 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"}]
 
-def test_case3():
-    list1 = ['Pune\n', 'Mumbai\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n', 'Pune\n', 'Mumbai\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n']
-    list2 = ['Mumbai\n', 'Delhi\n', 'Kochi\n', 'Mumbai\n', 'Delhi\n', 'Kochi\n']
-    list3 = fileCompare26nov.func(list1, list2)
-    assert list3 == ['\n', 'Mumbai\n', '\n','Delhi\n', 'Kochi<mismatch>\n', '\n', 'Mumbai\n','\n', 'Delhi\n', 'Kochi<mismatch>\n']
+    list3 = main_func(list1, list2)
+    assert list3 == [{'id': '333l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     {'id': '334l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'}]
 
-def test_case4():
-    list1 = ['Pune\n', 'Mumbai\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n', 'Pune\n', 'Mumbai\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n']
-    list2 = ['Mumbai\n', 'Delhi\n', 'Kochi\n']
-    list3 = fileCompare26nov.func(list1, list2)
-    assert list3 == ['\n', 'Mumbai\n', '\n', 'Delhi\n', '\n', '\n', '\n', '\n', '\n', 'Kochi<mismatch>\n']
 
-def test_case5():
-    list1 = ['Pune\n','Maharashtra\n','Mumbai\n','Pune\n','Maharashtra\n','Delhi\n','Patna\n']
-    list2 = ['Mumbai\n','Delhi\n','Kochi\n','Kochi2\n','Kochi3\n']
-    list3 = fileCompare26nov.func(list1,list2)
-    assert list3 == ['\n', '\n', 'Mumbai\n', '\n', '\n', 'Delhi\n', 'Kochi<mismatch>\n', 'Kochi2<mismatch>\n', 'Kochi3<mismatch>\n']
+def test_exact_same_list1_greater():
+    list1 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"335l","raw_log_time":8,"evt_order":0,"user":"abc"}]
 
-def test_case6():
-    list1 = ['Pune\n', 'Maharashtra\n', 'Mumbai\n', 'Pune\n', 'Maharashtra\n', 'Delhi\n', 'Patna\n', 'Kochi\n']
-    list2 = ['Mumbai\n', 'Delhi\n', 'Kochi\n', 'Kochi2\n', 'Kochi3\n']
-    list3 = fileCompare26nov.func(list1,list2)
-    assert list3 == ['\n', '\n', 'Mumbai\n', '\n', '\n', 'Delhi\n', '\n', 'Kochi\n', 'Kochi2<mismatch>\n', 'Kochi3<mismatch>\n']
+    list2 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"}]
+
+    list3 = main_func(list1, list2)
+    assert list3 == [{'id': '333l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     {'id': '334l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},'\n']
+
+
+def test_exact_same_list2_greater():
+    list1 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"335l","raw_log_time":8,"evt_order":0,"user":"abc"}]
+
+    list2 = [{"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"334l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"335l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"336l","raw_log_time":8,"evt_order":0,"user":"abc"}]
+
+    list3 = main_func(list1, list2)
+    assert list3 == [{'id': '333l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     {'id': '334l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     {'id': '335l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     "{'id': '336l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'}<mismatch>"]
+
+
+def test_differ():
+    list1 = [{"id":"323l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"333l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"338l","raw_log_time":8,"evt_order":0,"user":"abc"}]
+
+    list2 = [{"id":"323l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"338l","raw_log_time":8,"evt_order":0,"user":"abc"},
+             {"id":"343l","raw_log_time":8,"evt_order":0,"user":"abc"}]
+
+    list3 = main_func(list1, list2)
+    assert list3 == [{'id': '323l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     '\n',
+                     {'id': '338l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     "{'id': '343l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'}<mismatch>"]
+
+
+def test_differ_list1_greater():
+    list1 = [{"id": "323l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "333l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "338l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "344l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "346l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "348l", "raw_log_time": 8, "evt_order": 0, "user": "abc"}]
+
+    list2 = [{"id": "323l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "338l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "343l", "raw_log_time": 8, "evt_order": 0, "user": "abc"},
+             {"id": "347l", "raw_log_time": 8, "evt_order": 0, "user": "abc"}]
+
+    list3 = main_func(list1, list2)
+
+    assert list3 == [{'id': '323l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     '\n',
+                     {'id': '338l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'},
+                     "{'id': '343l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'}<mismatch>",
+                     '\n',
+                     "{'id': '347l', 'raw_log_time': 8, 'evt_order': 0, 'user': 'abc'}<mismatch>",]
+
 
