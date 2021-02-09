30bee6303d25d0e7a4a79863e65372982d11e594
Yashbansal411 <yashbansal411@gmail.com>
2021-02-09 09:11:48 -0800

    keys missing from inputs issue solved

--- file_compare.py
+++ file_compare.py
@@ -4,15 +4,20 @@ import json
 from operator import itemgetter
 total_threads = 0
 
+main_keys = ['id', 'e_type', 'source', 'vendor', 'time', 'raw_log_time', 'evt_order', 'fields']
+field_keys = ['table_name', '-msg-type', 'host', 'db_operation', 'src_ip', 'dest_host', 'database_name', 'app', 'time', 'error_code', 'user', 'db_query']
+
 
 def read_input(file_address):
     df1 = pd.read_json(file_address, orient='records', lines=True, chunksize=10000)
     try:
         input1 = df1.read()
     except ValueError:
-        #inputs not in proper format
         return -1
     dict1 = input1.to_dict('records')
+    for i in dict1:
+        if list(i.keys()) != main_keys or list(i['fields'].keys()) != field_keys:
+            return -2
     del input1
     return dict1
 
@@ -72,9 +77,9 @@ def adjust_mismatch(list2, list3, list2Index, second, lastAppend):
 def list_to_file(list3, code):
     if not os.path.isdir('./output_files'):
         os.mkdir('./output_files')
-    if list3 == -1:
+    if isinstance(list3, str):
         f = open('output_files/'+code+'.txt', 'w')
-        f.write("inputs were wrong, inputs must be in proper json format")
+        f.write(list3)
     else:
         with open('output_files/'+code+'.txt','w') as f:
             for i in list3:
@@ -86,7 +91,7 @@ def list_to_file(list3, code):
 
 
 def persist_file_length(list3, code):
-    if list3 == -1:
+    if isinstance(list3, str):
         total_number_of_lines = 1
     else:
         total_number_of_lines = len(list3)
@@ -110,8 +115,19 @@ def main_code(file1_address, file2_address, code):
     total_threads += 1
     list1 = read_input(file1_address)
     list2 = read_input(file2_address)
-    if list1 == -1 or list2 == -1:
-        list3 = -1
+    if isinstance(list1, int) or isinstance(list2, int): # check if any inputs are not in json or key missing
+        if list1 == -1 and list2 == -1:
+            list3 = 'both inputs are not in proper json format'
+        elif list1 == -2 and list2 == -2:
+            list3 = 'keys are missing from both input files'
+        elif list1 == -1:
+            list3 = 'file1 not in proper json format'
+        elif list2 == -1:
+            list3 = 'file2 not in proper json format'
+        elif list1 == -2:
+            list3 = 'keys from file1 are missing'
+        elif list2 == -2:
+            list3 = 'keys from file2 are missing'
     else:
         sorted_list1 = sorted(list1, key=itemgetter('raw_log_time'))
         sorted_list2 = sorted(list2, key=itemgetter('raw_log_time'))
